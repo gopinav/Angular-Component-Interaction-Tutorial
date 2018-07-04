44f923fa7fee47520930456b99fdf8991003359b
Vishwas <gopinav5@gmail.com>
2018-07-04 21:03:06 +0530

    Input Binding

---
+++ child-demo/src/app/child/child.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  templateUrl: './child.component.html',
+  styleUrls: ['./child.component.css']
+})
+export class ChildComponent implements OnInit {
+
+  @Input('loggedIn') loginFlag = false;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
