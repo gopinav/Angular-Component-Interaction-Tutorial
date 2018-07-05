4d155eb954787a62e5be615a7622d7fa8900bc04
Vishwas <gopinav5@gmail.com>
2018-07-05 22:39:42 +0530

    Input with Getters and Setters

--- child-demo/src/app/child/child.component.ts
+++ child-demo/src/app/child/child.component.ts
@@ -7,7 +7,24 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ChildComponent implements OnInit {
 
-  @Input('loggedIn') loginFlag = false;
+  private _loggedIn: boolean;
+  message: string;
+
+  get loggedIn(): boolean {
+    return this._loggedIn;
+  }
+
+  @Input()
+  set loggedIn(value: boolean) {
+    this._loggedIn = value;
+    if (value === true) {
+      this.message = 'Welcome back Vishwas!';
+    } else {
+      this.message = 'Please log in';
+    }
+  }
+
+  // @Input() loggedIn: boolean;
   constructor() { }
 
   ngOnInit() {
