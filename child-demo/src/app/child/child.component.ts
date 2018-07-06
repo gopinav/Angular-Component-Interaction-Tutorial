e197f249f6aa2a5de86a6dd76a9089c2d948e64f
Vishwas <gopinav5@gmail.com>
2018-07-06 23:27:18 +0530

    ngOnChanges method for tracking input  property change

--- child-demo/src/app/child/child.component.ts
+++ child-demo/src/app/child/child.component.ts
@@ -1,33 +1,40 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.css']
 })
-export class ChildComponent implements OnInit {
+export class ChildComponent implements OnChanges {
 
-  private _loggedIn: boolean;
+  // private _loggedIn: boolean;
   message: string;
 
-  get loggedIn(): boolean {
-    return this._loggedIn;
-  }
+  // get loggedIn(): boolean {
+  //   return this._loggedIn;
+  // }
 
-  @Input()
-  set loggedIn(value: boolean) {
-    this._loggedIn = value;
-    if (value === true) {
-      this.message = 'Welcome back Vishwas!';
+  // @Input()
+  // set loggedIn(value: boolean) {
+  //   this._loggedIn = value;
+  //   if (value === true) {
+  //     this.message = 'Welcome back Vishwas!';
+  //   } else {
+  //     this.message = 'Please log in';
+  //   }
+  // }
+
+  @Input() loggedIn: boolean;
+  constructor() { }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log(changes);
+    const loggedInValue = changes['loggedIn'];
+    if (loggedInValue.currentValue === true) {
+      this.message = 'Welcome back Vishwas!!';
     } else {
       this.message = 'Please log in';
     }
-  }
-
-  // @Input() loggedIn: boolean;
-  constructor() { }
-
-  ngOnInit() {
   }
 
 }
