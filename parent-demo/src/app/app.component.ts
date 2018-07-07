c31beb0c30012007816f9079dbdb74ff347373c3
Vishwas <gopinav5@gmail.com>
2018-07-07 13:54:43 +0530

    Emitting and Binding Child Events

--- parent-demo/src/app/app.component.ts
+++ parent-demo/src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  greet(name: string) {
+    alert('Hey ' + name);
+  }
 }
--- parent-demo/src/app/child/child.component.ts
+++ parent-demo/src/app/child/child.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChildComponent implements OnInit {
 
+  @Output() greetEvent = new EventEmitter();
+  name = 'Codevolution';
   constructor() { }
 
   ngOnInit() {
+  }
+
+  callParentGreet() {
+    this.greetEvent.emit(this.name);
   }
 
 }
