d0973408fde353bdff02b68b4a777984f74533b7
Vishwas <gopinav5@gmail.com>
2018-07-08 08:45:57 +0530

    ViewChild decorator

--- template-demo/src/app/app.component.ts
+++ template-demo/src/app/app.component.ts
@@ -1,17 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   pageTitle = 'Angular Component Interaction';
   imgUrl = 'https://picsum.photos/200';
   count = 0;
   name: string;
   userName: string;
   private _customerName: string;
+  @ViewChild('nameRef') nameElementRef: ElementRef;
+
+  ngAfterViewInit() {
+    this.nameElementRef.nativeElement.focus();
+    console.log(this.nameElementRef);
+  }
 
   get customerName(): string {
     return this._customerName;
